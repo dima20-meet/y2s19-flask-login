daaed7a4aa4029d3316b1cf8cde985571e4baa20
myname21-meet <myname21@meet.mit.edu>
2019-07-31 08:28:20 -0400

    flask-login(barely)

--- databases.py
+++ databases.py
@@ -12,6 +12,7 @@ def add_user(name,secret_word):
     """Add a user to the DB."""
     user = User(username=name)
     #there is a line of code missing here, what else does a user need?
+    user.hash_password(secret_word)
     session.add(user)
     session.commit()
 
@@ -19,4 +20,10 @@ def get_user(username):
     """Find the first user in the DB, by their username."""
     return session.query(User).filter_by(username=username).first()
 
+def add_user(fav_food):
+    food = User(food=fav_food)
+    session.add(food)
+    session.commit()    
 
+def fav_food(food):
+	session.query(User).filter_by(fav_food = food).first().fav_food = food
